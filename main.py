e7aaa116a93e55daded9c47416730566df683d3a
Yazeed Almuqwishi <yazeed.almuqwishi@gmail.com>
2021-10-02 10:26:18 +0300

    Return geojson dataframe with indicators

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+with open("dammam_districts_with_indicators.geojson") as f:
+    dammam_indicators = json.load(f)
 
-@app.get("/")
-async def root():
-    with open("districts.json") as f:
-        districts = json.load(f)
-        return districts
+
+# Will support other cities later
+@app.get("/dammam")
+async def dammam():
+    return dammam_indicators
