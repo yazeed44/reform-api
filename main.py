7d21c6d15a61c84123a5fb13fe40bd4118ed50a7
Yazeed Almuqwishi <yazeed.almuqwishi@gmail.com>
2021-10-02 09:43:34 +0000

    Handling CORS

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import json
 
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 
 with open("dataframe.geojson") as f:
     dataframe = json.load(f)
